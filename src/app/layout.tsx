4676f67f45331b2821c422b9622ba9a844c97cc7
junyeongc <junyeongc1000@gmail.com>
2026-01-13 14:37:15 +0900

    타이틀 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import '@/shared/styles/globals.css'
 import { AuthProvider } from '@/features/auth'
 
 export const metadata: Metadata = {
-  title: 'ClassDuo - 학습의 새로운 파트너',
+  title: 'ClassDuo - 대학생 공부중독 앱',
   description: 'AI 기반 학습 보조 플랫폼',
   icons: {
     icon: '/student_pavicon.png',
